6c7bb7dfd268a02d79911d366eb42f8acb1d04d0
David Mills <31848108+DMills94@users.noreply.github.com>
2024-05-18 19:40:32 +0100

    Add seasonal artefacts (#9)
    * Add seasonal artefacts (template structure, need images + adding to other layouts
    
    * update other arena formats for seasonal artefacts
    
    * feat: add seasonal artefact images
    
    * Refactoring artefacts selector component (#10)
    
    * refactor: move artefact selector to own component
    
    * refactor: map artefacts object for the artefact builder
    
    * refactor: move artefacts to own file outside of images
    
    * feat: autoscroll on artefact dropdown when device window small

--- components/layouts/Arena3.tsx
+++ components/layouts/Arena3.tsx
@@ -1,14 +1,7 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import CharacterSlot from "@/components/CharacterSlot";
 import Image from "next/image";
-import { spellImages, tekImages } from "@/lib/images";
+import { tekImages } from "@/lib/images";
+import ArtefactSelector from "@/components/ArtefactSelector";
 
 export default function Arena3Layout(props: {
   onCharacterSlotClick: (index: number) => void;
@@ -77,79 +70,7 @@ export default function Arena3Layout(props: {
         />
       </div>
       <div className="grid grid-cols-4 gap-2">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <div className="h-16 w-14">
-              <Image
-                src={spellImages[props.spell]}
-                alt={props.spell}
-                className="object-contain"
-              />
-            </div>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-48">
-            <DropdownMenuSeparator />
-            <DropdownMenuRadioGroup
-              value={props.spell}
-              onValueChange={props.setSpell}
-            >
-              <DropdownMenuRadioItem value="awakening">
-                <Image
-                  height={36}
-                  src={spellImages["awakening"]}
-                  alt="awakening"
-                  className="mr-2"
-                />
-                Awakening
-              </DropdownMenuRadioItem>
-              <DropdownMenuRadioItem value="blazing">
-                <Image
-                  height={36}
-                  src={spellImages["blazing"]}
-                  alt="blazing"
-                  className="mr-2"
-                />
-                Blazing
-              </DropdownMenuRadioItem>
-              <DropdownMenuRadioItem value="confining">
-                <Image
-                  height={36}
-                  src={spellImages["confining"]}
-                  alt="confining"
-                  className="mr-2"
-                />
-                Confining
-              </DropdownMenuRadioItem>
-              <DropdownMenuRadioItem value="enlightening">
-                <Image
-                  height={36}
-                  src={spellImages["enlightening"]}
-                  alt="enlightening"
-                  className="mr-2"
-                />
-                Enlightening
-              </DropdownMenuRadioItem>
-              <DropdownMenuRadioItem value="ironwall">
-                <Image
-                  height={36}
-                  src={spellImages["ironwall"]}
-                  alt="ironwall"
-                  className="mr-2"
-                />
-                Ironwall
-              </DropdownMenuRadioItem>
-              <DropdownMenuRadioItem value="starshard">
-                <Image
-                  height={36}
-                  src={spellImages["starshard"]}
-                  alt="starshard"
-                  className="mr-2"
-                />
-                Starshard
-              </DropdownMenuRadioItem>
-            </DropdownMenuRadioGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <ArtefactSelector active={props.spell} onChange={props.setSpell} />
 
         <div
           className="h-16 w-16 grid grid-cols-1 place-items-center opacity-30"
