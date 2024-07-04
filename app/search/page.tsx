1c7009df27815dc851d5a74234a26cf2984421e6
Kyle <thekylemontag@gmail.com>
2024-07-04 06:47:00 +0000

    trunk fmt

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,13 +1,11 @@
 import Search from "@/components/Search";
 
 export default async function SearchPage() {
-    const cmsData = await (
-        await fetch(
-            `https://simplejsoncms.com/api/${process.env.NEXT_PUBLIC_SIMPLEJSONCMS_ID}`,
-        )
-    ).json();
+  const cmsData = await (
+    await fetch(
+      `https://simplejsoncms.com/api/${process.env.NEXT_PUBLIC_SIMPLEJSONCMS_ID}`,
+    )
+  ).json();
 
-    return (
-        <Search cmsData={cmsData} />
-    );
+  return <Search cmsData={cmsData} />;
 }
--- components/Search.tsx
+++ components/Search.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { FormEvent, useState } from "react";
 import { Button } from "@/components/ui/button";
