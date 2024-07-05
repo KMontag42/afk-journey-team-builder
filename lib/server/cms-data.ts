b0dcc10371461e8a98fe5d6ada2b538c8037f572
Kyle <thekylemontag@gmail.com>
2024-07-05 00:51:15 +0000

    add server-only to cms-data.ts

--- lib/server/cms-data.ts
+++ lib/server/cms-data.ts
@@ -1,3 +1,5 @@
+import 'server-only';
+
 export async function getCmsData() {
     const jsonData = await (
         await fetch(`https://simplejsoncms.com/api/${process.env.CHARACTERS_SIMPLEJSONCMS_ID}`)
