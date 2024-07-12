14f7f284c8a460ecac6f78009e4c1d4ce6ea8eb6
Kyle <thekylemontag@gmail.com>
2024-07-12 19:25:02 +0000

    start of admin protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,14 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const protectedPaths = createRouteMatcher(["/admin(.*)"]);
+
+export default clerkMiddleware((auth, req) => {
+  const { userId, sessionClaims } = auth();
+
+  if (!userId && protectedPaths(req) && sessionClaims?.role !== "admin") {
+    return auth().redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
