f54a8a9da224a7619303380f06992116c2679e9b
SwiftieHusband <l.anthony@live.com>
2024-07-14 22:53:36 -0600

    refactor

--- lib/nav-menu-builder.ts
+++ lib/nav-menu-builder.ts
@@ -1,23 +1,17 @@
 export function getSections(baseUrl: string, json: JSON): any[] {
-  let sections: {
-    name: string;
-    items: { title: string; href: string; description: string }[];
-  }[] = [];
-  let listItems: { title: string; href: string; description: string }[] = [];
-
-  Object.entries(json).map(([key, data]: [string, any]) => {
-    listItems = [];
-    Object.entries(data).map(([key, data]: [string, any]) => {
-      listItems.push({
+  const sections = Object.entries(json).map(([key, data]: [string, any]) => {
+    const listItems = Object.entries(data).map(
+      ([key, data]: [string, any]) => ({
         title: data.title,
         href: baseUrl + key,
         description: data.description,
-      });
-    });
-    sections.push({
+      }),
+    );
+    return {
       name: key.charAt(0).toUpperCase() + key.slice(1),
       items: listItems,
-    });
+    };
   });
+
   return sections;
 }
