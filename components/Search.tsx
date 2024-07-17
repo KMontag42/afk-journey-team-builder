1d70a936b9ffa2901ef7cab9cf1f74170f74444f
SwiftieHusband <l.anthony@live.com>
2024-07-16 22:52:46 -0600

    Search page 3 columns, or 2, or 1.

--- components/Search.tsx
+++ components/Search.tsx
@@ -26,8 +26,11 @@ export default function Search({ cmsData }: { cmsData: any }) {
   };
 
   return (
-    <div className="md:px-0 md:container md:w-[40vw] mx-auto">
-      <form onSubmit={handleSearch} className="flex flex-col gap-2 px-2">
+    <div className="flex flex-col items-center md:px-0 md:container mx-auto">
+      <form
+        onSubmit={handleSearch}
+        className="flex md:w-[40vw] flex-col gap-2 px-2"
+      >
         <Label htmlFor="name" className="text-2xl">
           Search
         </Label>
@@ -42,18 +45,19 @@ export default function Search({ cmsData }: { cmsData: any }) {
 
       <Separator className="my-4" />
 
-      {loading && <p>Loading...</p>}
-
-      <ScrollArea className="mt-4 md:px-4">
-        {results.map((result: any) => (
-          <FormationCard
-            key={result.id.toString()!}
-            data={result as any}
-            className="mb-4"
-            cmsData={cmsData}
-          />
-        ))}
-      </ScrollArea>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-4 my-4 md:px-4">
+          {results.map((result: any) => (
+            <FormationCard
+              key={result.id.toString()!}
+              data={result as any}
+              cmsData={cmsData}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
