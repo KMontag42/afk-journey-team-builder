6cb80e8e0de461370f40bc6db17adb251a86117e
SwiftieHusband <l.anthony@live.com>
2024-08-07 00:42:18 -0600

    ummm.. the revert didn't work? weird.

--- lib/server/drizzle.ts
+++ lib/server/drizzle.ts
@@ -2,9 +2,5 @@ import "server-only";
 
 import { drizzle } from "drizzle-orm/libsql";
 import { turso } from "@/lib/server/turso";
-import { formations, votes } from "@/drizzle/schema";
-import { formationsRelations, votesRelations } from "@/drizzle/relations";
 
-export const drizzleClient = drizzle(turso, {
-  schema: { formations, votes, votesRelations, formationsRelations },
-});
+export const drizzleClient = drizzle(turso);
