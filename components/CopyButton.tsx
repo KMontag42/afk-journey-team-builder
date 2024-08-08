b924d067c3598d01dc1e1adf2f6ac0c828c78a94
Kyle <thekylemontag@gmail.com>
2024-08-08 16:25:35 +0000

    show checkmark for already copied codes

--- components/CopyButton.tsx
+++ components/CopyButton.tsx
@@ -2,17 +2,55 @@
 
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+import { useState, useEffect } from "react";
+import { Check } from "lucide-react";
 
 export default function CopyButton({ label }: { label: string }) {
+  const [copied, setCopied] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const already_copied = localStorage.getItem("already_copied");
+    if (!already_copied) {
+      localStorage.setItem("already_copied", JSON.stringify({}));
+    } else {
+      const label_already_copied = JSON.parse(already_copied)[label];
+
+      setCopied(label_already_copied);
+    }
+    setLoaded(true);
+  }, [label]);
+
+  // we don't want to render the button until we have checked if it was already copied
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <Button
       variant="outline"
       onClick={() => {
         navigator.clipboard.writeText(label);
         toast.success("Copied to clipboard!");
+
+        // we can use ! here because we know that the key exists
+        const already_copied = localStorage.getItem("already_copied")!;
+        const parsed = JSON.parse(already_copied);
+        localStorage.setItem(
+          "already_copied",
+          JSON.stringify({ ...parsed, [label]: true }),
+        );
+        setCopied(true);
       }}
     >
-      {label}
+      {copied ? (
+        <>
+          <Check />
+          {label}
+        </>
+      ) : (
+        label
+      )}
     </Button>
   );
 }
