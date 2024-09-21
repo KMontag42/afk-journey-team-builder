7cac8565d656aab57942404dedf872cee4c19239
SwiftieHusband <l.anthony@live.com>
2024-09-20 20:15:54 -0600

    hide Patreon

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -36,7 +36,11 @@ export default async function AboutPage() {
       <p className="text-center">Consider supporting us by donating here:</p>
       <div className="flex flex-row flex-wrap items-center justify-center gap-4">
         <Button variant="analytica" asChild>
-          <Link target="_blank" href="https://www.patreon.com/analytica">
+          <Link
+            className="hidden"
+            target="_blank"
+            href="https://www.patreon.com/analytica"
+          >
             <Image
               src={tekImages.patreonIcon}
               alt="KoFi Cup"
