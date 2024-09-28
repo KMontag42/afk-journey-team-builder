579235b8e5920b1c855b2aa0d2f21e026bb5d01a
Kyle <thekylemontag@gmail.com>
2024-09-28 01:45:52 +0000

    use loading component for loading.tsx files

--- app/codes/loading.tsx
+++ app/codes/loading.tsx
@@ -1,32 +1,5 @@
-"use client";
-
-import { motion } from "framer-motion";
-import Image from "next/image";
-import AnalyitcaOfficialLogoV4 from "@/public/AnalyticaOfficialLogoV4.png";
+import LoadingComponent from "@/components/Loading";
 
 export default function Loading() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
-      <div className="relative w-64 h-16 mb-8">
-        <motion.div
-          className="text-white text-2xl"
-          animate={{
-            opacity: [1, 0.5, 1],
-          }}
-          transition={{
-            duration: 1.5,
-            repeat: Infinity,
-            ease: "easeInOut",
-          }}
-        >
-          <Image
-            src={AnalyitcaOfficialLogoV4}
-            alt="Analytica Logo"
-            layout="fill"
-            objectFit="contain"
-          />
-        </motion.div>
-      </div>
-    </div>
-  );
+  return <LoadingComponent />;
 }
