770849047b7556ea4dd7caf0190ce22934e09279
Kyle Montag <KMontag42@users.noreply.github.com>
2024-09-28 17:19:12 -0400

    allow CORS for GET/OPTIONS requests (#51)

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,27 @@ const nextConfig = {
       },
     ],
   },
+  headers: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
